9331ff8b8b8b3b5250f09e5adfc84845cfa71cea
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2023-12-13 00:53:55 +0100

    fix: #54 change input to YML. Fixes

--- tests/functions/parse-change.test.js
+++ tests/functions/parse-change.test.js
@@ -1,75 +1,18 @@
 const { parseChange } = require('../../functions/parse-change')
 const fs = require('fs')
-const testData = [
-  {
-    input: 'fix: #123 solved error when querying DB',
-    output: {
-      type: 'fix',
-      releaseAssociated: 'patch',
-      scopes: [],
-      description: '#123 solved error when querying DB',
-      originalCommit: 'fix: #123 solved error when querying DB'
-    }
-  },
-  {
-    input: 'feat(src): #234 added new endpoint for deleting user',
-    output: {
-      type: 'feat',
-      releaseAssociated: 'minor',
-      scopes: ['src'],
-      description: '#234 added new endpoint for deleting user',
-      originalCommit: 'feat(src): #234 added new endpoint for deleting user'
-    }
-  },
-  {
-    input: 'break(src,tfm): #564 updated API endpoints structure',
-    output: {
-      type: 'break',
-      releaseAssociated: 'major',
-      scopes: ['src', 'tfm'],
-      description: '#564 updated API endpoints structure',
-      originalCommit: 'break(src,tfm): #564 updated API endpoints structure'
-    }
-  },
-  {
-    input: 'docs(src,tfm): #231 Updated readme for tfm and src',
-    output: {
-      type: 'docs',
-      releaseAssociated: 'none',
-      scopes: ['src', 'tfm'],
-      description: '#231 Updated readme for tfm and src',
-      originalCommit: 'docs(src,tfm): #231 Updated readme for tfm and src'
-    }
-  },
-  {
-    input: 'refactor(tfm): #234 simplified terraform solution',
-    output: {
-      type: 'refactor',
-      releaseAssociated: 'none',
-      scopes: ['tfm'],
-      description: '#234 simplified terraform solution',
-      originalCommit: 'refactor(tfm): #234 simplified terraform solution'
-    }
-  },
-  {
-    input: '#2345 Commit that do not follow conventional commits',
-    output: {
-      type: '',
-      releaseAssociated: 'none',
-      scopes: [],
-      description: '#2345 Commit that do not follow conventional commits',
-      originalCommit: '#2345 Commit that do not follow conventional commits'
-    }
-  }
-]
+const yaml = require('js-yaml')
 
 describe('changes are properly parsed', () => {
+  const testDataContent = fs.readFileSync('tests/functions/parse-change.test.data.json')
+  const testData = JSON.parse(testDataContent)
+
   testData.forEach(data => {
     it(`change:\n\t${data.input}\nis parsed into:\n\t${JSON.stringify(data.output, null, 2)}`,
       () => {
-        const defaultConf = JSON.parse(fs.readFileSync('default-config.json'))
-        const changeTypes = defaultConf.changeTypes
-        const change = parseChange(data.input, changeTypes)
+        const defaultConfigContent = fs.readFileSync('default-config.yml')
+        const defaultConf = yaml.load(defaultConfigContent)
+        const commitsConfig = defaultConf.commits
+        const change = parseChange(data.input, commitsConfig)
         expect(change).toEqual(data.output)
       })
   })
