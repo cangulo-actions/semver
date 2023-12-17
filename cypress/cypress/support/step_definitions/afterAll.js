98e3e9798e6d800e37a473add22915d810ae1f1b
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2023-12-18 00:58:46 +0100

    refactor: #67 improved beforeAll and afterAll to avoid keeping pending PRs. Fixes #67 (#68)

--- cypress/cypress/support/step_definitions/afterAll.js
+++ cypress/cypress/support/step_definitions/afterAll.js
@@ -1,25 +1,30 @@
 import { AfterAll } from '@badeball/cypress-cucumber-preprocessor'
 
 AfterAll(function () {
-  const enabled = Cypress.env('AFTER_ALL_RESET_REPO_ENABLED')
+  const resetRepoEnabled = Cypress.env('AFTER_ALL_RESET_REPO_ENABLED')
+  const closePRsEnabled = Cypress.env('AFTER_ALL_CLOSE_ANY_PR')
 
-  if (!enabled) {
-    console.log('trigger reset repo workflow skipped')
-    return
-  }
-
-  const waitTimeWorkflow = Cypress.env('GH_WORKFLOW_RESET_REPO_TIMEOUT')
-  const workflowId = 'reset-repo.yml'
-  const workflowParams = {
-    ref: 'main',
-    inputs: {
-      'semver-version': 'main',
-      'enable-ci-workflow': false
+  if (resetRepoEnabled) {
+    const waitTimeWorkflow = Cypress.env('GH_WORKFLOW_RESET_REPO_TIMEOUT')
+    const workflowId = 'reset-repo.yml'
+    const workflowParams = {
+      ref: 'main',
+      inputs: {
+        'semver-version': 'main',
+        'enable-ci-workflow': false
+      }
     }
+
+    // eslint-disable-next-line cypress/unsafe-to-chain-command
+    cy
+      .triggerWorkflow({ workflowId, workflowParams })
+      .wait(waitTimeWorkflow)
+  } else {
+    console.log('trigger reset repo workflow skipped')
   }
 
-  // eslint-disable-next-line cypress/unsafe-to-chain-command
-  cy
-    .triggerWorkflow({ workflowId, workflowParams })
-    .wait(waitTimeWorkflow)
+  if (closePRsEnabled) {
+    // if any PR is pending because of a previous failing execution, close it
+    cy.closeAnyPendingPR()
+  }
 })
--- cypress/cypress/support/step_definitions/beforeAll.js
+++ cypress/cypress/support/step_definitions/beforeAll.js
@@ -1,25 +1,29 @@
 import { BeforeAll } from '@badeball/cypress-cucumber-preprocessor'
 
 BeforeAll(function () {
-  const enabled = Cypress.env('BEFORE_ALL_RESET_REPO_ENABLED')
-  if (!enabled) {
-    console.log('trigger reset repo workflow skipped')
-    return
-  }
+  const resetRepoEnabled = Cypress.env('BEFORE_ALL_RESET_REPO_ENABLED')
+  const closePRsEnabled = Cypress.env('BEFORE_ALL_CLOSE_ANY_PR')
 
-  const waitTimeWorkflow = Cypress.env('GH_WORKFLOW_RESET_REPO_TIMEOUT')
-  const semverBranchName = Cypress.env('SEMVER_BRANCH')
-  const workflowId = 'reset-repo.yml'
-  const workflowParams = {
-    ref: 'main',
-    inputs: {
-      'semver-version': semverBranchName,
-      'enable-ci-workflow': false
+  if (resetRepoEnabled) {
+    const waitTimeWorkflow = Cypress.env('GH_WORKFLOW_RESET_REPO_TIMEOUT')
+    const semverBranchName = Cypress.env('SEMVER_BRANCH')
+    const workflowId = 'reset-repo.yml'
+    const workflowParams = {
+      ref: 'main',
+      inputs: {
+        'semver-version': semverBranchName,
+        'enable-ci-workflow': false
+      }
     }
+
+    // eslint-disable-next-line cypress/unsafe-to-chain-command
+    cy
+      .triggerWorkflow({ workflowId, workflowParams })
+      .wait(waitTimeWorkflow)
   }
 
-  // eslint-disable-next-line cypress/unsafe-to-chain-command
-  cy
-    .triggerWorkflow({ workflowId, workflowParams })
-    .wait(waitTimeWorkflow)
+  if (closePRsEnabled) {
+    // if any PR is pending because of a previous failing execution, close it
+    cy.closeAnyPendingPR()
+  }
 })
