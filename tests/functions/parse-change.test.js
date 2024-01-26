828a64e2da3d3690b1cd8550fba65c3cf4438f32
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2024-01-26 23:45:33 +0100

    test: clean test and delete config.default.yml (#97)

--- tests/functions/parse-change.test.js
+++ tests/functions/parse-change.test.js
@@ -1,14 +1,21 @@
 const { parseChange } = require('../../functions/parse-change')
 const fs = require('fs')
-const yaml = require('js-yaml')
+const yml = require('js-yaml')
+const Ajv = require('ajv')
 
 describe('changes are properly parsed', () => {
   const testDataContent = fs.readFileSync('tests/functions/parse-change.test.data.json')
   const testData = JSON.parse(testDataContent)
 
+  const schemaPath = 'config.schema.yml'
+  const schemaContent = fs.readFileSync(schemaPath)
+  const schema = yml.load(schemaContent)
+  const defaultConf = {}
+  const ajv = new Ajv({ useDefaults: true }) // add default values to the config properties
+  const addDefaultValues = ajv.compile(schema)
+  addDefaultValues(defaultConf)
+
   // arrange
-  const defaultConfigContent = fs.readFileSync('config.default.yml')
-  const defaultConf = yaml.load(defaultConfigContent)
   const commitsConfig = defaultConf.commits
 
   testData
